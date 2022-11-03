48dd756c29e8f3f657f576ed7aa5089ff3d77cab
Tolga Tuna Acar <110130709+Tolgatunaacar@users.noreply.github.com>
2022-11-04 01:07:20 +0300

    Add files via upload

---
+++ Fikstur/src/fixtureGenerator/Main.java
@@ -0,0 +1,22 @@
+package fixtureGenerator;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        ArrayList<Team> teams = new ArrayList<>();
+        teams.add(new Team("Galatasaray"));
+        teams.add(new Team("Fenerbahçe"));
+        teams.add(new Team("Trabzonspor"));
+        teams.add(new Team("Beşiktaş"));
+        teams.add(new Team("Bursa"));
+        teams.add(new Team("Başakşehir"));
+
+
+        ArrayList<Fixture> fixtureArrayList = Fixture.FixtureGenerator(teams);
+        Fixture.print(fixtureArrayList);
+
+    }
+}
